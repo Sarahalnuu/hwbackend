22b955b1e35e6c57c50891c6a55cc9b0888648d2
Sarahalnuu <Saraotb4400@gmail.com>
2022-11-15 13:24:42 +0300

    homework1

---
+++ calculator.ts
@@ -0,0 +1,46 @@
+// var prompt1 = require('prompt'); 
+
+// class Arithmetic
+// {
+// operator(a: number, b: number)
+// {
+// let add: number, sub: number, mul: number, div: number, mod: number; 
+// add = a + b; 
+// sub = a - b; 
+// mul = a * b;
+// div = a / b;
+// let span = document.createElement("span");
+// span.innerText = "\nAddition Of Number is-
+// >" + add + "\n" + "Sub Of
+// Number is -> "+sub+"\
+// n "+" 
+// Multiplication Of Number is -> "+mul+"\
+// n "+" 
+// Division 
+// Of Number is -> "+div+"\
+// n "+" 
+// Modulus Of Number is -> "+mod+"\ 
+// n "+"
+// document.body.appendChild(span);
+// } 
+// } 
+// window.onload = () => 
+// { 
+// var a: number, b: number; 
+// a = parseInt(prompt("Enter A First Number")); 
+// b = parseInt(prompt("Enter A Second Number"));
+// var greeter = new Arithmetic();
+// var span = document.createElement("span");
+// span.style.color = "green";
+// span.innerText = "First number is-
+// >" + a + "\n" + "Second Number is-
+// >" + b + "\n";
+// document.body.appendChild(span);
+// greeter.operator(a, b);
+// };
+
+// let x = 2;
+// let y = 3;
+
+// let z = x + y;
+// console.log(z);
---
+++ index.ts
@@ -0,0 +1,15 @@
+interface User {
+    name: string;
+    age: number;
+    Specialization: string;
+    threebestfriends: string;
+}
+
+const user1: User = {
+    name: "Sarah" ,
+    age: 22 ,
+    Specialization: "CS",
+    threebestfriends: "Manal, worod, kholod",
+}
+
+console.log(user1);
